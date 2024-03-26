99f7c65c853397f44c0c2ce5c52929ef9a2e24e4
avanronaufaladi <avaroadi@gmail.com>
2024-03-26 11:06:36 +0700

    memasukkan fungsi penjumlahan,pengurangan,perkalian,pembagian.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,3 +3,22 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
+int penjumlahan(int a, int b)
+{
+	return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+	return a - b;
+}
+
+int perkalian(int a, int b)
+{
+	return a * b;
+}
+
+float pembagian(int a, int b)
+{
+	return a / b;
+}

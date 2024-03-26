f0ecc669c9d9c411dfcabbe1067227960aad63fe
avanronaufaladi <avaroadi@gmail.com>
2024-03-26 11:08:15 +0700

    membuat menu kalkulator sederhana

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -30,3 +30,19 @@ void inputdata()
 	cout << "\nmasukkan bilangan kedua: ";
 	cin >> bilangan2;
 }
+
+int main()
+{
+	int pilihan;
+	do
+	{
+		system("CLS");
+		cout << "menu kalkulator sederhana " << endl;
+		cout << "1. penjumlahan" << endl;
+		cout << "2. pengurangan" << endl;
+		cout << "3. perkalian " << endl;
+		cout << "4. pembagian" << endl;
+		cout << "5. keluar" << endl;
+		cout << "masukkan pilihan: ";
+		cin >> pilihan;
+
